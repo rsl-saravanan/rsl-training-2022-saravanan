f594f48bb10bb75154d567a6ce02fd0e8a43252b
rsl-saravanan <saravanan.g@rsllearning.com>
2022-07-07 20:36:45 +0530

    Day 3 : Views Part - 2
    On Main Activity added three buttons

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,18 +3,49 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         setContentView(R.layout.activity_main);  // Linear Layout
-   //    setContentView(R.layout.activity_relativelayout);
-      //  setContentView(R.layout.activity_constraintfile);
+        setContentView(R.layout.activity_main);
+
+        Button linearButton = findViewById(R.id.linearButton);
+        Button relativeButton = findViewById(R.id.relativeButton);
+        Button constraintButton = findViewById(R.id.constraintButton);
+
+        linearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(@NonNull View view) {
+                Intent intent = new Intent(MainActivity.this, LinearActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        relativeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(@NonNull View view) {
+                Intent intent = new Intent(MainActivity.this, RelativeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        constraintButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(@NonNull View view) {
+                Intent intent = new Intent(MainActivity.this, ConstraintActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
-}
+    }
+
